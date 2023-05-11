087ceb1077e8a80488f63147042c2336cc8a6cec
vaarun18 <varunsadananda108@gmail.com>
2023-05-11 22:42:20 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	deleted:    src/App.css 	modified:   src/App.jsx 	deleted:    src/assets/react.svg 	modified:   src/index.css 	new file:   src/pages/Login.jsx 	new file:   src/pages/Room.jsx 	new file:   tailwind.config.js

---
+++ src/pages/Room.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID = 1375406431;
+    const serverSecret = "c31cb8ef7a14967793675e0f2bd5598a";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Varun"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
